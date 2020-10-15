15cb410d40d15440a39fa61a4e15fd0d51d73a67
Miguel Munoz <memg92@gmail.com>
2020-10-15 19:46:01 -0300

    finished recipes and instructions queries

--- data-access-layer/instructions-repository.js
+++ data-access-layer/instructions-repository.js
@@ -1,24 +1,22 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 let Instruction;
 let moduleError;
 
 try {
-  const db = require('../models');
+  const db = require("../models");
   ({ Instruction } = db);
   if (Instruction === undefined) {
-    moduleError = 'It looks like you need to generate the Instruction model.';
+    moduleError = "It looks like you need to generate the Instruction model.";
   }
 } catch (e) {
   console.error(e);
-  if (e.message.includes('Cannot find module')) {
-    moduleError = 'It looks like you need initialize your project.';
+  if (e.message.includes("Cannot find module")) {
+    moduleError = "It looks like you need initialize your project.";
   } else {
     moduleError = `An error was raised "${e.message}". Check the console for details.`;
   }
 }
 /* Don't change code above this line ******************************************/
-
-
 
 async function createNewInstruction(specification, recipeId) {
   // Use the findAll method of the Instruction object to find all the
@@ -26,11 +24,23 @@ async function createNewInstruction(specification, recipeId) {
   //
   // Use the create method of the Instruction object to create a new object and
   // return it using the maximum listOrder from the query just before this.
+  let instructions = await Instruction.findAll({
+    where: { recipeId: recipeId },
+    order: [["listOrder", "DESC"]],
+    limit: 1,
+  });
+
+  let listOrderMax = instructions[0] ? instructions[0].toJSON().listOrder : 0;
+
+  let instruction = await Instruction.create({
+    specification: specification,
+    recipeId: recipeId,
+    listOrder: listOrderMax,
+  });
+  return instruction;
   //
   // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
 }
-
-
 
 /* Don't change code below this line ******************************************/
 module.exports = {
