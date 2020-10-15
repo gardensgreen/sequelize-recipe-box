9077a506fbc2222312aa94fca0de3b0938394c7f
Daniel TIllero <danieltillero67@gmail.com>
2020-10-15 18:49:39 -0400

    add ingredients

--- data-access-layer/ingredients-repository.js
+++ data-access-layer/ingredients-repository.js
@@ -1,35 +1,45 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 let Ingredient;
 let moduleError;
 
 try {
-  const db = require('../models');
-  ({ Ingredient } = db);
-  if (Ingredient === undefined) {
-    moduleError = 'It looks like you need to generate the Ingredient model.';
-  }
+    const db = require("../models");
+    ({ Ingredient } = db);
+    if (Ingredient === undefined) {
+        moduleError =
+            "It looks like you need to generate the Ingredient model.";
+    }
 } catch (e) {
-  console.error(e);
-  if (e.message.includes('Cannot find module')) {
-    moduleError = 'It looks like you need initialize your project.';
-  } else {
-    moduleError = `An error was raised "${e.message}". Check the console for details.`;
-  }
+    console.error(e);
+    if (e.message.includes("Cannot find module")) {
+        moduleError = "It looks like you need initialize your project.";
+    } else {
+        moduleError = `An error was raised "${e.message}". Check the console for details.`;
+    }
 }
 /* Don't change code above this line ******************************************/
 
+async function createNewIngredient(
+    amount,
+    recipeId,
+    measurementUnitId,
+    foodStuff
+) {
+    // Use the create method of the Ingredient object to create a new object.
 
+    let ingredient = await Ingredient.create({
+        amount: amount,
+        recipeId: recipeId,
+        measurementUnitId: measurementUnitId,
+        foodStuff: foodStuff,
+    });
 
-async function createNewIngredient(amount, recipeId, measurementUnitId, foodStuff) {
-  // Use the create method of the Ingredient object to create a new object.
-  //
-  // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
+    return ingredient;
+    // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
 }
-
-
 
 /* Don't change code below this line ******************************************/
 module.exports = {
-  createNewIngredient,
-  loadingDbError: moduleError,
+    createNewIngredient,
+    loadingDbError: moduleError,
 };
